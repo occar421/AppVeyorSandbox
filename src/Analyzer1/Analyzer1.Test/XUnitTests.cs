f0d67d3d1aedfd463bf764ad0ecb62b24221fef1
MasuqaT <occar@hotmail.co.jp>
2016-03-12 16:25:33 +0900

    Add unit test by xUnit

---
+++ src/Analyzer1/Analyzer1.Test/XUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Analyzer1.Test
+{
+	public class XUnitTests
+	{
+		[Fact]
+		public void SuccessTest()
+		{
+			Assert.True(true);
+		}
+
+		[Fact]
+		public void FailTest()
+		{
+			Assert.True(false);
+		}
+	}
+}
