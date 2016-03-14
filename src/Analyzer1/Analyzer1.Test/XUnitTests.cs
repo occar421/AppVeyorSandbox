e87b857f7e4ddf922992ad48536ed64d3883a1a1
MasuqaT <occar@hotmail.co.jp>
2016-03-14 11:18:27 +0900

    Change MsTest to xUnit

--- src/Analyzer1/Analyzer1.Test/XUnitTests.cs
+++ src/Analyzer1/Analyzer1.Test/XUnitTests.cs
@@ -1,19 +1,80 @@
-﻿using Xunit;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using TestHelper;
+
+using Xunit;
 
 namespace Analyzer1.Test
 {
-	public class XUnitTests
+	public class XUnitTests : CodeFixVerifier
 	{
+		//No diagnostics expected to show up
 		[Fact]
-		public void SuccessTest()
+		public void Empty()
 		{
-			Assert.True(true);
+			var test = @"";
+
+			VerifyCSharpDiagnostic(test);
 		}
 
+		//Diagnostic and CodeFix both triggered and checked for
 		[Fact]
-		public void FailTest()
+		public void Normal()
 		{
-			Assert.False(false);
+			var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+        }
+    }";
+			var expected = new DiagnosticResult
+			{
+				Id = "Analyzer1",
+				Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
+				Severity = DiagnosticSeverity.Warning,
+				Locations =
+					new[] {
+							new DiagnosticResultLocation("Test0.cs", 11, 15)
+						}
+			};
+
+			VerifyCSharpDiagnostic(test, expected);
+
+			var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+			VerifyCSharpFix(test, fixtest);
+		}
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider()
+		{
+			return new Analyzer1CodeFixProvider();
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+		{
+			return new Analyzer1Analyzer();
 		}
 	}
 }
